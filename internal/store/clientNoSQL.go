90f45b834cae40a886498620eadd09752e2d9df7
assalyk <karim.assaly@epitech.eu>
2021-04-07 20:41:58 +0200

    Added the IP location & 3 translations EN ES FR in the go routine PUT function

--- internal/store/clientNoSQL.go
+++ internal/store/clientNoSQL.go
@@ -34,49 +34,28 @@ func (c *ClientNoSQL) Get(ctx context.Context, opts Options) ([]model.Translatio
 
 	val, err := c.rdb.Do(ctx, "HGET", "location", opts.IP).Result()
 	if err != nil {
-		return model.Translation{}, fmt.Errorf("Not found IP")
+		return nil, fmt.Errorf("Not found IP")
 	}
-<<<<<<< HEAD
 	fmt.Printf("this is val = %v\n", val)
-	return model.Translation{}, nil
-=======
-	fmt.Println(val)
-	return []model.Translation{}, nil
->>>>>>> ee7b9b7c30e9fce544c0bf0ec3307320e8359354
+	return nil, nil
 }
 
 func (c *ClientNoSQL) Put(ctx context.Context, translations Translations, ip []model.Location) error {
 
 	var err *redis.Cmd
 	wg := sync.WaitGroup{}
-	wg.Add(500)
-<<<<<<< HEAD
-	//nb_iplocations := len(ip) / 500
+	wg.Add(3000)
+	nb_iplocations := len(ip) / 500
 	nb_translationsfr := len(translations.TranslationFR) / 500
+	nb_translationsen := len(translations.TranslationEN) / 500
+	nb_translationses := len(translations.TranslationES) / 500
 
-=======
-	portion := len(ip) / 500
->>>>>>> ee7b9b7c30e9fce544c0bf0ec3307320e8359354
 	for i := 0; i < 500; i++ {
-		//go func(i int, nb_iplocations int, ip []model.Location) error {
-		//	defer wg.Done()
-		//
-		//	for j := i * nb_iplocations; j < (i+1)*nb_iplocations; j++ {
-		//		err = c.rdb.Do(ctx, "HSET", "location", ip[j].Address, ip[j].UUID)
-		//		if err != nil {
-		//			fmt.Println(err)
-		//			return err.Err()
-		//		}
-		//
-		//	}
-		//	return nil
-		//}(i, nb_iplocations, ip)
-		go func(i int, nb_translationsfr int, translationsFR []interface{}) error {
+		go func(i int, nb_iplocations int, ip []model.Location) error {
 			defer wg.Done()
 
-			for j := i * nb_translationsfr; j < (i+1)*nb_translationsfr; j++ {
-				mTransFR, _ := json.Marshal(translationsFR[j])
-				err = c.rdb.Do(ctx, "HSET", "translationFR", j, mTransFR)
+			for j := i * nb_iplocations; j < (i+1)*nb_iplocations; j++ {
+				err = c.rdb.Do(ctx, "HSET", "location", ip[j].Address, ip[j].UUID)
 				if err != nil {
 					fmt.Println(err)
 					return err.Err()
@@ -84,7 +63,52 @@ func (c *ClientNoSQL) Put(ctx context.Context, translations Translations, ip []m
 
 			}
 			return nil
-		}(i, nb_translationsfr, translations.TranslationFR)
+		}(i, nb_iplocations, ip)
+		go func(i int, nb_translationsfr int, translations Translations) error {
+			defer wg.Done()
+			translationsFR := translations.TranslationFR
+
+			for j := i * nb_translationsfr; j < (i+1)*nb_translationsfr; j++ {
+				mTransFR, _ := json.Marshal(translationsFR[j])
+				err = c.rdb.Do(ctx, "HSET", "translationFR", translationsFR[j].UUID, mTransFR)
+				if err != nil {
+					fmt.Println(err)
+					return err.Err()
+				}
+
+			}
+			return nil
+		}(i, nb_translationsfr, translations)
+		go func(i int, nb_translationses int, translations Translations) error {
+			defer wg.Done()
+			translationsES := translations.TranslationES
+
+			for j := i * nb_translationses; j < (i+1)*nb_translationses; j++ {
+				mTransES, _ := json.Marshal(translationsES[j])
+				err = c.rdb.Do(ctx, "HSET", "translationES", translationsES[j].UUID, mTransES)
+				if err != nil {
+					fmt.Println(err)
+					return err.Err()
+				}
+
+			}
+			return nil
+		}(i, nb_translationses, translations)
+		go func(i int, nb_translationsen int, translations Translations) error {
+			defer wg.Done()
+			translationsEN := translations.TranslationEN
+
+			for j := i * nb_translationsen; j < (i+1)*nb_translationsfr; j++ {
+				mTransEN, _ := json.Marshal(translationsEN[j])
+				err = c.rdb.Do(ctx, "HSET", "translationEN", translationsEN[j].UUID, mTransEN)
+				if err != nil {
+					fmt.Println(err)
+					return err.Err()
+				}
+
+			}
+			return nil
+		}(i, nb_translationsen, translations)
 	}
 
 	wg.Wait()
