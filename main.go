9568d249543867271aa182b1267e260fb02dc7c3
Paul Prost <paul.prost@epitech.eu>
2021-04-12 19:49:39 +0200

    refactor sqlx

--- main.go
+++ main.go
@@ -10,29 +10,30 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func initRoute(router *gin.Engine, client *store.Client, redisClient *store.ClientNoSQL) {
-	router.GET("/location", http.GetLocation(network.Get(redisClient)))
-	router.PUT("/locations", http.PutLocation(network.Put(redisClient)))
+func initRoute(router *gin.Engine, client store.Store, redisClient *store.ClientNoSQL) {
+	router.GET("/location", http.GetLocation(network.Get(client)))
+	router.PUT("/locations", http.PutLocation(network.Put(client)))
 }
 
 var ctx = context.Background()
 
 func main() {
 
-	clt := store.NewClient()
-	rclt := store.NewNSQLClient()
-
-	if err := clt.Init(); err != nil {
-		log.Fatal("error while init postgres client")
+	var client store.Store
+	client, err := store.NewSQLClient()
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	rclt := store.NewNSQLClient()
 	if err := rclt.Init(); err != nil {
 		log.Fatal("error while init redis client")
 	}
 	router := gin.Default()
 
-	initRoute(router, clt, rclt)
+	initRoute(router, client, rclt)
 
-	err := router.Run(":8081")
+	err = router.Run(":8081")
 	if err != nil {
 		log.Fatal("error while running the router")
 	}
